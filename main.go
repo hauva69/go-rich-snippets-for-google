266658188b62470e09df3037fd5647c1f851d235
Ari Mäkelä <ari.makela@iki.fi>
2015-12-15 15:20:50 +0200

    go fmt

--- main.go
+++ main.go
@@ -10,23 +10,22 @@ type Author struct {
 }
 
 type AggregateRating struct {
-	Type string `json:"@type"`
+	Type        string `json:"@type"`
 	RatingValue string `json:"ratingValue"`
 	ReviewCount string `json:"reviewCount"`
 }
 
 type RichSnippet struct {
-	Context string `json:"@context"`
-	Type string `json:"@type"`
-	Name string `json:"name"`
-	ImageUrl string `json:"image"`
-	DatePublished string `json:"datePublished"`
-	Description string `json:"description"`
+	Context         string          `json:"@context"`
+	Type            string          `json:"@type"`
+	Name            string          `json:"name"`
+	ImageUrl        string          `json:"image"`
+	DatePublished   string          `json:"datePublished"`
+	Description     string          `json:"description"`
 	AggregateRating AggregateRating `json:"aggregateRating"`
-	PreparationTime string `json:"prepTime"`
-	CookingTime string `json:"cookTime"`
-	TotalTime string `json:"totalTime"`
-
+	PreparationTime string          `json:"prepTime"`
+	CookingTime     string          `json:"cookTime"`
+	TotalTime       string          `json:"totalTime"`
 }
 
 func main() {
