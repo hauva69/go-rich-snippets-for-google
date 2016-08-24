4920c7874bb22b5a6c375674d22da078debebbbd
Ari Makela <ari.makela@iki.fi>
2016-08-24 11:41:57 +0300

    first commit

---
+++ frontend/app/rich-snippet-for-recipe.ts
@@ -0,0 +1,11 @@
+export class RichSnippetForRecipe {
+    name: string;
+    recipeCategory: string;
+    imageUrl: string;
+    datePublished: string;
+    description: string;
+    aggregateRating: number;
+    preparationTime: string;
+    cookingTime: string;
+    totalTime: string;
+}
